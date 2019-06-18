7ad3c12917c46d29f8e1b7d26edc9529ae8c6d92
Ilya Gulko <iigulko99@gmail.com>
2019-06-18 17:43:10 +0300

    Edited 1st lab, changed naming

--- lab1.py
+++ lab1.py
@@ -1,22 +1,42 @@
+#!/usr/bin/env python                                       
+# -*- coding: utf-8 -*-                                     
+
 from random import seed
 from random import randint
+from time import time
 
-seed(1)
 
-rand_nums = [randint(0, 99) for i in range(50)]
-print("Generated list: ", list)
+def randomizer(seedkey):
+    seed(seedkey)
+    rand_list = [randint(0, 99) for i in range(50)]
 
-new_list = []
+    return rand_list
 
-try:
-    min_num = int(input("Enter minimum number: "))
-    max_num = int(input("Enter maximum number: "))
-    for i, val in enumerate(rand_nums):
-        if min_num <= val <= max_num:
-            print("list[" + str(i) + "] => " + str(val))
-            new_list.append(i)
 
-except:
-    print("Incorrect input")
+def main():
+    randomized_list = randomizer(seedkey=int(time()))
+    print("Generated list: ", randomized_list)
 
-print("Result list: ", new_list)
+    new_list = []
+
+    try:
+        min_num = int(input("Enter minimum number: "))
+        max_num = int(input("Enter maximum number: "))
+        if max_num <= min_num:
+            print("Wrong numspace, try again.")
+            exit(1)
+        for i, val in enumerate(randomized_list):
+            if min_num <= val <= max_num:
+                print("list[" + str(i) + "] => " + str(val))
+                new_list.append(i)
+        print("Result list: ", new_list)
+    except ValueError:
+        print("Incorrect input, try again.")
+        exit(2)
+    except Exception as e:
+        print("Unexpected exception: ", e)
+        exit(127)
+
+
+if __name__ == "__main__":
+    main()
