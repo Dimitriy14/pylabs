259e59992c56ff1c6f52022960504a7779e95ad6
Dimitriy14 <yankovskiydy98@gmail.com>
2019-07-02 18:00:47 +0300

    Added second lab to week2

--- week2/2.3.1.1/lab1.py
+++ week2/2.3.1.1/lab1.py
@@ -5,13 +5,13 @@ import requests
 def get_ticket():
     requests.packages.urllib3.disable_warnings()
 
-    api_url = "https://sandboxapicem.cisco.com/api/v1/ticket"
+    api_url = "https://devnetsbx-netacad-apicem-3.cisco.com/api/v1/ticket"
     headers = {
         "content-type": "application/json"
     }
     body_json = {
         "username": "devnetuser",
-        "password": "Cisco123!"
+        "password": "Xj3BDqbU"
     }
 
     resp=requests.post(api_url,json.dumps(body_json),headers=headers,verify=False)
